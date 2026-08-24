76e3686ea305070286e3017b5e893f833ff16346
Eduardo Pacheco <eduardo@eapprojetos.com.br>
2026-08-24 00:24:42 -0300

    fix: remove unused Column import in notification-read entity

--- backend/src/notifications/notification-read.entity.ts
+++ backend/src/notifications/notification-read.entity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity('notification_reads')
 export class NotificationRead {
